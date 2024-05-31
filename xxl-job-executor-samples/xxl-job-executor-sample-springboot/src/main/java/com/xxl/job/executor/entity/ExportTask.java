0abd51bf89fe9c993201ba89fdd3d21bc334edee
liufei <liufei@liufei.info>
2024-05-31 15:30:41 +0800

    提交

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/entity/ExportTask.java
@@ -0,0 +1,68 @@
+package com.xxl.job.executor.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import java.io.Serializable;import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@TableName(value = "t_export_task")
+public class ExportTask extends Model {
+    private static final long serialVersionUID = 1L;
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 导出类型 excel csv
+     */
+    @TableField(value = "`type`")
+    private String type;
+
+    /**
+     * 接收人邮箱
+     */
+    @TableField(value = "email")
+    private String email;
+
+    /**
+     * 要导出的sql
+     */
+    @TableField(value = "`sql`")
+    private String sql;
+
+    /**
+     * 是否删除 0-正常 1-删除
+     */
+    @TableField(value = "is_delete")
+    private Integer isDelete;
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "created")
+    private LocalDateTime created;
+
+    public static final String COL_ID = "id";
+
+    public static final String COL_TYPE = "type";
+
+    public static final String COL_EMAIL = "email";
+
+    public static final String COL_SQL = "sql";
+
+    public static final String COL_IS_DELETE = "is_delete";
+
+    public static final String COL_CREATED = "created";
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+}
---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/mapper/ExportTaskMapper.java
@@ -0,0 +1,7 @@
+package com.xxl.job.executor.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xxl.job.executor.entity.ExportTask;
+
+public interface ExportTaskMapper extends BaseMapper<ExportTask> {
+}
